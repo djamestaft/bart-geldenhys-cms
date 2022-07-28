99eebba03a8cb5ab93d02df48cbf736edf613b69
Devon Taft <devon.taft@rank.com>
2022-07-28 15:36:04 +0200

    stuff

---
+++ src/api/home-blurb/controllers/home-blurb.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  home-blurb controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-blurb.home-blurb');
---
+++ src/api/home-blurb/routes/home-blurb.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-blurb router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-blurb.home-blurb');
